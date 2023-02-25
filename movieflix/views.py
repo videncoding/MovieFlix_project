fda90363389091a2a3ecf7ab2b00a3015c2098b2
KxBooo <2740897K@student.gla.ac.uk>
2023-02-25 21:37:47 +0000

    testkxb

--- movieflix/views.py
+++ movieflix/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 from django.shortcuts import render
 from django.http.response import HttpResponse
-
+#test
 def index(request):
     return HttpResponse('Hello')
